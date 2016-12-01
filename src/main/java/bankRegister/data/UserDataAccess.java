1ab9376457b0b0d69d1068f3908589d57a3287e9
Peter Bullock <bullock@white-paper-media.de>
2016-12-01 12:50:53 +0100

    Registration SQL

--- src/main/java/bankRegister/data/UserDataAccess.java
+++ src/main/java/bankRegister/data/UserDataAccess.java
@@ -20,23 +20,48 @@ public class UserDataAccess extends DataAccess {
     }
 
     public void addUser(String Nachname,  String Vorname, String PLZ, String Ort, String Strasse, String Geburtsdatum, String Passwort){
-
+        Integer Adresse_ID = 0;
 
         try {
             Connection conn = this.getConnection();
 
-            String sql = "SELECT kunde_id, Vorname, Nachname FROM kunden";
+            this.beginTransaction();
 
-            Statement s = conn.createStatement();
-            s.execute(sql);
-            ResultSet r = s.getResultSet();
+
+            String adresse_sql = "INSERT INTO adresse (Straße, Ort, PLZ, Land) VALUES (?,?,?,?)";
+
+            PreparedStatement adresse_s = conn.prepareStatement(adresse_sql);
+            adresse_s.setString(1, Strasse);
+            adresse_s.setString(2, Ort);
+            adresse_s.setString(3, PLZ);
+            adresse_s.setString(4, "DE");
+            adresse_s.execute();
+
+            String last_address_sql = "SELECT max(ID) AS ID FROM adresse";
+            Statement last_address = conn.createStatement();
+            last_address.execute(last_address_sql);
+            ResultSet r = last_address.getResultSet();
 
             while(r.next()){
-               // userList.add( new UserModel(r.getInt("kunde_id"), r.getString("Vorname"), r.getString("Nachname")));
+               Adresse_ID = r.getInt("ID");
             }
 
-        } catch (SQLException e) {
 
+
+            String sql = "INSERT INTO kunden (Vorname, Nachname, Geburtsdatum, Passwort, Adresse_ID) VALUES (?,?,?,?, ?)";
+
+            PreparedStatement s = conn.prepareStatement(sql);
+            s.setString(1, Vorname);
+            s.setString(2, Nachname);
+            s.setString(3, Geburtsdatum);
+            s.setString(4, Passwort);
+            s.setInt(5, Adresse_ID);
+            s.execute();
+
+            this.commitTransaction();
+
+        } catch (SQLException e) {
+            this.rollBackTransaction();
             e.printStackTrace();
 
         }finally {
